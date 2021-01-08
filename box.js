f9845048e2c6c58cc5f352d4af856ac833f43377
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-01-09 00:11:51 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,9 +1,9 @@
+  
 class Box {
   constructor(x, y, width, height) {
     var options = {
-        'restitution':0.8,
-        'friction':0.3,
-        'density':1.0
+        'restitution':1,
+       
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
@@ -13,13 +13,10 @@ class Box {
   }
   display(){
     var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
+   
     rectMode(CENTER);
     fill(255);
-    rect(0, 0, this.width, this.height);
-    pop();
+    rect(pos.x, pos.y, this.width, this.height);
+    
   }
 };
